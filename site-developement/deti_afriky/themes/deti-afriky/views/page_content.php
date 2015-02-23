11881922cf1600597db44c59c688dafd9236994e
unknown <V3502287@C3500100241.ness.com>
2015-02-23 12:15:29 +0100

    Repaired some bugs in article, edited article view

--- site-developement/deti_afriky/themes/deti-afriky/views/page_content.php
+++ site-developement/deti_afriky/themes/deti-afriky/views/page_content.php
@@ -6,13 +6,6 @@
                 <div class="col-xs-12 col-lg-12">
                     <ion:article:title tag="h2"/>
                     <ion:article:subtitle tag="p" class="text-muted" />
-                    <!-- intro image -->
-                    <ion:article:medias type="picture" limit="1">
-                        <div class="intro-img">
-                            <img src="<ion:media:src />" alt="<ion:media:alt />" class="img-responsive"/>
-                        </div>
-                    </ion:article:medias >
-
                     <div class="article-para">
                         <ion:article:content />
                     </div>
