09beef2dd94e59774dbe3c28b71db9c15480479a
unknown <V3502286@C3500100334.ness.com>
2015-02-23 12:17:10 +0100

    pull

---
+++ site-developement/deti_afriky/application/logs/log-2015-02-23.php
@@ -0,0 +1,8 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2015-02-23 12:14:22 --> Language file contains no data: language/sk/user_lang
+ERROR - 2015-02-23 12:14:23 --> Language file contains no data: language/sk/user_lang
+ERROR - 2015-02-23 12:14:24 --> Language file contains no data: language/sk/user_lang
+ERROR - 2015-02-23 12:14:26 --> Language file contains no data: language/sk/user_lang
+ERROR - 2015-02-23 12:14:50 --> Language file contains no data: language/sk/user_lang
+ERROR - 2015-02-23 12:14:51 --> Language file contains no data: language/sk/user_lang
