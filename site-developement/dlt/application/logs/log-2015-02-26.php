0b5069e68fcec95c5c5cea1dd3b312a9bc245c78
Franky238 <frank238238@gmail.com>
2015-02-26 12:34:03 +0100

    Added untracked files

--- site-developement/dlt/application/logs/log-2015-02-26.php
+++
@@ -1,3 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2015-02-26 12:12:38 --> Severity: Notice  --> Only variable references should be returned by reference C:\xampp\htdocs\nesskdckosiciam\site-developement\dlt\system\core\Common.php 244
