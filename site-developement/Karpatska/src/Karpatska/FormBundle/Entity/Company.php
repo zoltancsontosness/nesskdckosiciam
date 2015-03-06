9f8dd41f4d49dcd1323e6a90a556706ad7c56132
unknown <V3502287@C3500100241.ness.com>
2015-03-06 10:42:58 +0100

    repaired bug with not hashing password

--- site-developement/Karpatska/src/Karpatska/FormBundle/Entity/Company.php
+++ site-developement/Karpatska/src/Karpatska/FormBundle/Entity/Company.php
@@ -388,7 +388,7 @@ class Company
      */
     public function setPassword($password)
     {
-        $this->password = $password;
+        $this->password = md5($password);
         return $this;
     }
     /**
