c56b60aa1f63d3ab7adf4ce08e5bde45483c81d1
Franky238 <frank238238@gmail.com>
2015-03-12 19:24:34 +0100

    Save of form now working

--- site-developement/Karpatska/src/Karpatska/FormBundle/Controller/FormController.php
+++ site-developement/Karpatska/src/Karpatska/FormBundle/Controller/FormController.php
@@ -3,33 +3,38 @@
 namespace Karpatska\FormBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Karpatska\FormBundle\Entity\RealAnswer;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 
 class FormController extends Controller
 {
     /**
-     * @Route("/form/{formId}", name="_form")
+     * @Route("company/form/{formId}", name="_form")
      * @Template()
      */
     public function formAction($formId, Request $request)
     {
-        /*
         $em = $this->getDoctrine()->getManager();
-        */
+        $companyIco = $this->get('security.context')->getToken()->getUsername();
+        $company = $this->getDoctrine()->getRepository("KarpatskaFormBundle:Company")->findOneByIco($companyIco);
+
         if($_POST)
         {
-            foreach($_POST as $questionId => $questionText) {
+            foreach($_POST as $questionId => $answerText)
+            {
                 $answer = new RealAnswer();
                 $answer->setFormId($formId);
                 $answer->setQuestionId($questionId);
-                $answer->setQuestionText($questionText);
-                //$answer->setComapnyId(); TODO vyber id prave lognuteho usera
-
+                $answer->setAnswerText($answerText);
+                $answer->setCompanyId($company->getId());
                 $em->persist($answer);
-                $em->flush();
             }
+            $em->flush();
+
+            return new Response("Formulár bol odoslaný");
         }
 
         $form = $this->getDoctrine()->getRepository('KarpatskaFormBundle:Form')->find($formId);
---
+++ site-developement/Karpatska/src/Karpatska/FormBundle/Entity/RealAnswer.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace Karpatska\FormBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * RealAnswer
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class RealAnswer
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="form_id", type="integer")
+     */
+    private $formId;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="company_id", type="integer")
+     */
+    private $companyId;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="question_id", type="integer")
+     */
+    private $questionId;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="answer_text", type="text")
+     */
+    private $answerText;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set formId
+     *
+     * @param integer $formId
+     * @return RealAnswer
+     */
+    public function setFormId($formId)
+    {
+        $this->formId = $formId;
+
+        return $this;
+    }
+
+    /**
+     * Get formId
+     *
+     * @return integer 
+     */
+    public function getFormId()
+    {
+        return $this->formId;
+    }
+
+    /**
+     * Set companyId
+     *
+     * @param integer $companyId
+     * @return RealAnswer
+     */
+    public function setCompanyId($companyId)
+    {
+        $this->companyId = $companyId;
+
+        return $this;
+    }
+
+    /**
+     * Get companyId
+     *
+     * @return integer 
+     */
+    public function getCompanyId()
+    {
+        return $this->companyId;
+    }
+
+    /**
+     * Set questionId
+     *
+     * @param integer $questionId
+     * @return RealAnswer
+     */
+    public function setQuestionId($questionId)
+    {
+        $this->questionId = $questionId;
+
+        return $this;
+    }
+
+    /**
+     * Get questionId
+     *
+     * @return integer 
+     */
+    public function getQuestionId()
+    {
+        return $this->questionId;
+    }
+
+    /**
+     * Set answerText
+     *
+     * @param string $answerText
+     * @return RealAnswer
+     */
+    public function setAnswerText($answerText)
+    {
+        $this->answerText = $answerText;
+
+        return $this;
+    }
+
+    /**
+     * Get answerText
+     *
+     * @return string 
+     */
+    public function getAnswerText()
+    {
+        return $this->answerText;
+    }
+}
