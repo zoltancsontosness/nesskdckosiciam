dd0813fe2ca058ef1ca13419530f22e875a240b4
Franky238 <frank238238@gmail.com>
2015-04-02 01:07:20 +0200

    New db dump

--- site-developement/Karpatska/db_dump/karpatska.sql
+++ site-developement/Karpatska/db_dump/karpatska.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Hostiteľ: 127.0.0.1
--- Čas generovania: So 28.Mar 2015, 18:15
+-- Čas generovania: Št 02.Apr 2015, 01:06
 -- Verzia serveru: 5.6.21
 -- Verzia PHP: 5.6.3
 
@@ -26,6 +26,7 @@ SET time_zone = "+00:00";
 -- Štruktúra tabuľky pre tabuľku `answers`
 --
 
+DROP TABLE IF EXISTS `answers`;
 CREATE TABLE IF NOT EXISTS `answers` (
 `id` int(11) NOT NULL,
   `answerType` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
@@ -95,6 +96,7 @@ INSERT INTO `answers` (`id`, `answerType`, `question_id`, `answerText`) VALUES
 -- Štruktúra tabuľky pre tabuľku `company`
 --
 
+DROP TABLE IF EXISTS `company`;
 CREATE TABLE IF NOT EXISTS `company` (
 `id` int(11) NOT NULL,
   `ico` int(11) NOT NULL,
@@ -107,7 +109,7 @@ CREATE TABLE IF NOT EXISTS `company` (
   `account_num` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `repr_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `repr_email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `dic` int(11) DEFAULT NULL,
+  `dic` int(11) NOT NULL,
   `password` varchar(300) COLLATE utf8_unicode_ci NOT NULL,
   `salt` longtext COLLATE utf8_unicode_ci NOT NULL,
   `repr_phone` varchar(15) COLLATE utf8_unicode_ci NOT NULL
@@ -119,10 +121,10 @@ CREATE TABLE IF NOT EXISTS `company` (
 
 INSERT INTO `company` (`id`, `ico`, `address`, `county`, `region`, `type`, `bank_name`, `bank_address`, `account_num`, `repr_name`, `repr_email`, `dic`, `password`, `salt`, `repr_phone`) VALUES
 (4, 12345678, 'Moldavská cesta 24', 'Košice', 'Košický', 'sro', 'Banka', 'Hlavná 1', 'SK3302000000000000012351', 'Admin Adminovsky', 'frank238238@gmail.com', 1234567890, '846cc2fe8b93c6ac62f34aa3ad3c4eb7', '5ecf195bbd8c07f4c50f6e8ef6aae07c', '+421908483898'),
-(5, 87654321, 'Vyšný Čaj 15', 'Košice', 'Košický', 'nadácia', 'slsp', 'Domaca 4', 'SK3112000000001987426375', 'Frantisek Ferko', 'frantisek.ferko@ness.com', NULL, '846cc2fe8b93c6ac62f34aa3ad3c4eb7', '7df04f76fdf606e37a591b5654461f57', '+421908483898'),
-(6, 15975312, 'Vyšný Čaj 15', 'Košice', 'Košický', 'nadácia', 'slsp', 'Domaca 4', 'SK3112000000001987426375', 'Frantisek Ferko', 'frantisek.ferko@ness.com', NULL, '846cc2fe8b93c6ac62f34aa3ad3c4eb7', 'b3592778dd8e623ac660ba707f490ad6', '+421908483898'),
-(7, 12345679, 'Vysny caj 15', 'Kosice', 'Košický', 'nadácia', 'slsp', 'bratislavska 1, bratislava', 'SK3112000000001987426375', 'Fero Ferko', 'frantisek.ferko@ness.com', NULL, '846cc2fe8b93c6ac62f34aa3ad3c4eb7', '5ff194f0c4f06904679924220266d298', '+421908483898'),
-(9, 12345670, 'Vysny caj 15', 'Kosice', 'Košický', 'nadácia', 'slsp', 'bratislavska 1, bratislava', 'SK3112000000001987426375', 'Fero Ferko', 'frantisek.ferko@ness.com', NULL, '846cc2fe8b93c6ac62f34aa3ad3c4eb7', '65a2dcf233a52b7f53425c319d366955', '+421908483898');
+(5, 87654321, 'Vyšný Čaj 15', 'Košice', 'Košický', 'nadácia', 'slsp', 'Domaca 4', 'SK3112000000001987426375', 'Frantisek Ferko', 'frantisek.ferko@ness.com', 0, '846cc2fe8b93c6ac62f34aa3ad3c4eb7', '7df04f76fdf606e37a591b5654461f57', '+421908483898'),
+(6, 15975312, 'Vyšný Čaj 15', 'Košice', 'Košický', 'nadácia', 'slsp', 'Domaca 4', 'SK3112000000001987426375', 'Frantisek Ferko', 'frantisek.ferko@ness.com', 0, '846cc2fe8b93c6ac62f34aa3ad3c4eb7', 'b3592778dd8e623ac660ba707f490ad6', '+421908483898'),
+(7, 12345679, 'Vysny caj 15', 'Kosice', 'Košický', 'nadácia', 'slsp', 'bratislavska 1, bratislava', 'SK3112000000001987426375', 'Fero Ferko', 'frantisek.ferko@ness.com', 0, '846cc2fe8b93c6ac62f34aa3ad3c4eb7', '5ff194f0c4f06904679924220266d298', '+421908483898'),
+(9, 12345670, 'Vysny caj 15', 'Kosice', 'Košický', 'nadácia', 'slsp', 'bratislavska 1, bratislava', 'SK3112000000001987426375', 'Fero Ferko', 'frantisek.ferko@ness.com', 0, '846cc2fe8b93c6ac62f34aa3ad3c4eb7', '65a2dcf233a52b7f53425c319d366955', '+421908483898');
 
 -- --------------------------------------------------------
 
@@ -130,6 +132,7 @@ INSERT INTO `company` (`id`, `ico`, `address`, `county`, `region`, `type`, `bank
 -- Štruktúra tabuľky pre tabuľku `form`
 --
 
+DROP TABLE IF EXISTS `form`;
 CREATE TABLE IF NOT EXISTS `form` (
 `id` int(11) NOT NULL,
   `name` longtext COLLATE utf8_unicode_ci NOT NULL
@@ -149,66 +152,69 @@ INSERT INTO `form` (`id`, `name`) VALUES
 -- Štruktúra tabuľky pre tabuľku `question`
 --
 
+DROP TABLE IF EXISTS `question`;
 CREATE TABLE IF NOT EXISTS `question` (
 `id` int(11) NOT NULL,
   `questionText` longtext COLLATE utf8_unicode_ci NOT NULL,
   `form_id` int(11) DEFAULT NULL,
   `position` int(11) NOT NULL,
-  `type` varchar(50) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'question'
+  `type` varchar(50) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'question',
+  `json` varchar(5) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `description` longtext COLLATE utf8_unicode_ci
 ) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Sťahujem dáta pre tabuľku `question`
 --
 
-INSERT INTO `question` (`id`, `questionText`, `form_id`, `position`, `type`) VALUES
-(1, 'Organizácia (partner)', 1, 0, 'question'),
-(2, 'Kontaktná osoba', 1, 0, 'question'),
-(3, 'Pozícia v organizácii', 1, 0, 'question'),
-(4, 'Úloha partnera v projekte\r\n', 1, 0, 'question'),
-(5, 'Podpis štatutárneho zástupcu partnera', 1, 0, 'question'),
-(6, 'Podpis štatutárneho zástupcu žiadateľa o grant\r\n', 1, 0, 'question'),
-(7, 'Miesto', 1, 0, 'question'),
-(8, 'Forma žiadateľa', 2, 2, 'question'),
-(9, 'Názov projektu', 2, 1, 'question'),
-(10, 'Názov žiadateľa', 2, 2, 'question'),
-(11, 'Ulica', 2, 4, 'question'),
-(12, 'Obec/mesto', 2, 5, 'question'),
-(13, 'Okres', 2, 6, 'question'),
-(14, 'Telefón', 2, 7, 'question'),
-(15, 'IČO', 2, 8, 'question'),
-(16, 'Meno a priezvisko', 2, 10, 'question'),
-(17, 'Trvalý pobyt', 2, 11, 'question'),
-(18, 'Telefón', 2, 12, 'question'),
-(19, 'Email', 2, 13, 'question'),
-(20, 'Osoba zodpovedná za projekt', 2, 14, 'heading'),
-(21, 'Štatutár žiadateľa', 2, 9, 'heading'),
-(23, 'Žiadateľ', 2, 0, 'heading'),
-(25, 'Meno a priezvisko', 2, 15, 'question'),
-(26, 'Telefón', 2, 16, 'question'),
-(27, 'Email', 2, 17, 'question'),
-(28, 'Trvanie projektu', 2, 18, 'heading'),
-(29, 'Začiatok realizácie', 2, 18, 'question'),
-(30, 'Koniec realizácie', 2, 19, 'question'),
-(31, 'Rozpočet grantu', 2, 20, 'heading'),
-(32, 'Požadovaný grant', 2, 21, 'question'),
-(33, 'Celkový rozpočet projektu', 2, 22, 'question'),
-(34, 'Vklad žiadateľa ', 2, 23, 'question'),
-(35, 'Bankové údaje', 2, 24, 'heading'),
-(36, 'Názov účtu', 2, 25, 'question'),
-(37, 'IBAN', 2, 26, 'question'),
-(38, 'Názov banky', 2, 27, 'question'),
-(39, 'Adresa banky', 2, 28, 'question'),
-(40, 'Názov projektu', 2, 29, 'question'),
-(41, 'Zhrnutie projektu', 2, 30, 'question'),
-(42, 'Informácia o žiadateľovi', 2, 31, 'question'),
-(43, 'Informácie o mieste, kde budete projekt realizovať', 2, 32, 'question'),
-(44, 'Informácie o ľuďoch, pre ktorých budete projekt realizovať', 2, 33, 'question'),
-(45, 'Spôsoby informovania o projekte v tlači, televízii, na internete, atď.', 2, 34, 'question'),
-(46, 'Budúcnosť vášho projektu', 2, 35, 'question'),
-(47, 'Nejasnosti a riziká súvisiace s projektom ', 2, 36, 'question'),
-(48, 'Odkiaľ ste sa dozvedeli o 5. Výzve Fondu Karpatskej vandrovky MÁME RADI VÝCHOD?', 2, 37, 'question'),
-(49, 'Ciele a aktivity projektu ', 2, 38, 'question');
+INSERT INTO `question` (`id`, `questionText`, `form_id`, `position`, `type`, `json`, `description`) VALUES
+(1, 'Organizácia (partner)', 1, 0, 'question', NULL, NULL),
+(2, 'Kontaktná osoba', 1, 0, 'question', NULL, NULL),
+(3, 'Pozícia v organizácii', 1, 0, 'question', NULL, NULL),
+(4, 'Úloha partnera v projekte\r\n', 1, 0, 'question', NULL, NULL),
+(5, 'Podpis štatutárneho zástupcu partnera', 1, 0, 'question', NULL, NULL),
+(6, 'Podpis štatutárneho zástupcu žiadateľa o grant\r\n', 1, 0, 'question', NULL, NULL),
+(7, 'Miesto', 1, 0, 'question', NULL, NULL),
+(8, 'Forma žiadateľa', 2, 2, 'question', NULL, NULL),
+(9, 'Názov projektu', 2, 1, 'question', NULL, 'Názov projektu by mal byť stručný, výstižný a ľahko zapamätateľný.'),
+(10, 'Názov žiadateľa', 2, 2, 'question', NULL, NULL),
+(11, 'Ulica', 2, 4, 'question', NULL, NULL),
+(12, 'Obec/mesto', 2, 5, 'question', NULL, NULL),
+(13, 'Okres', 2, 6, 'question', NULL, NULL),
+(14, 'Telefón', 2, 7, 'question', NULL, NULL),
+(15, 'IČO', 2, 8, 'question', NULL, NULL),
+(16, 'Meno a priezvisko', 2, 10, 'question', NULL, NULL),
+(17, 'Trvalý pobyt', 2, 11, 'question', NULL, NULL),
+(18, 'Telefón', 2, 12, 'question', NULL, NULL),
+(19, 'Email', 2, 13, 'question', NULL, NULL),
+(20, 'Osoba zodpovedná za projekt', 2, 14, 'heading', NULL, NULL),
+(21, 'Štatutár žiadateľa', 2, 9, 'heading', NULL, NULL),
+(23, 'Žiadateľ', 2, 0, 'heading', NULL, NULL),
+(25, 'Meno a priezvisko', 2, 15, 'question', NULL, NULL),
+(26, 'Telefón', 2, 16, 'question', NULL, NULL),
+(27, 'Email', 2, 17, 'question', NULL, NULL),
+(28, 'Trvanie projektu', 2, 18, 'heading', NULL, NULL),
+(29, 'Začiatok realizácie', 2, 18, 'question', NULL, NULL),
+(30, 'Koniec realizácie', 2, 19, 'question', NULL, NULL),
+(31, 'Rozpočet grantu', 2, 20, 'heading', NULL, NULL),
+(32, 'Požadovaný grant', 2, 21, 'question', NULL, NULL),
+(33, 'Celkový rozpočet projektu', 2, 22, 'question', NULL, NULL),
+(34, 'Vklad žiadateľa ', 2, 23, 'question', NULL, NULL),
+(35, 'Bankové údaje', 2, 24, 'heading', NULL, NULL),
+(36, 'Názov účtu', 2, 25, 'question', NULL, NULL),
+(37, 'IBAN', 2, 26, 'question', NULL, NULL),
+(38, 'Názov banky', 2, 27, 'question', NULL, NULL),
+(39, 'Adresa banky', 2, 28, 'question', NULL, NULL),
+(40, 'Názov projektu', 2, 29, 'question', NULL, NULL),
+(41, 'Zhrnutie projektu', 2, 30, 'question', NULL, 'Základná informácia o projekte, ktorá stručne, jednoducho  a zrozumiteľne odpovedá na tieto otázky:  -	Prečo a pre koho  ste sa rozhodli váš projekt realizovať? -	Čo chcete vašim projektom dosiahnuť? -	Ako to chcete dosiahnuť?'),
+(42, 'Informácia o žiadateľovi', 2, 31, 'question', NULL, 'Stručná informácia o žiadateľovi, ktorá poskytne  aspoň tieto základné informácie:   - Zameranie organizácie a dátum jej vzniku. - Personálne obsadenie organizácie -  jej členská základňa, výkonná zložka. - Dokončené projekty a dosiahnuté výsledky.  - Zoznam projektov, na ktoré ste v minulosti získali podporu od Karpatskej nadácie aj s uvedením roku realizácie. '),
+(43, 'Informácie o mieste, kde budete projekt realizovať', 2, 32, 'question', NULL, 'Opíšte miesto (priestor, obec/mesto alebo región), kde budete realizovať váš projekt. Kde sa nachádza? Aké je veľké? Prečo ste si vybrali právve toto miesto? Aké sú silné a slabé stránky tohto miesta? Aký úžitok váš projekt tomuto miestu prinesie?'),
+(44, 'Informácie o ľuďoch, pre ktorých budete projekt realizovať', 2, 33, 'question', NULL, 'Opíšte cieľovú skupinu, na ktorú je projekt zameraný. Prečo ste sa zamerali na túto skupinu ľudí? Aké sú potreby týchto ľudí? Aký úžitok prinesie váš projekt týmto ľuďom? Koľkým ľuďom prinesie váš projekt úžitok? Kto bude využívať výsledky vášho projektu?'),
+(45, 'Spôsoby informovania o projekte v tlači, televízii, na internete, atď.', 2, 34, 'question', NULL, 'Stručne a prehľadne opíšte  -	ako (článok,rozhovor, leták, plagát,video,  ...),  -	kde (tlač, televízia, rozhlas, internet, ...),  -	kedy (min. mesiac a rok)  -	a koho (stručné a výstižné označenie skupiny ľudí, pre ktorú je informácia určená)   budete o vašom projekte počas jeho realizácie informovať.'),
+(46, 'Budúcnosť vášho projektu', 2, 35, 'question', NULL, 'Ak bude váš projekt pokračovat aj po skončení jeho realizácie, stručne opíšte vašu predstavu, váš plán na jeho ďalšie pokračovanie. Prípadne opíšte aj to, k akým vzdialenejším zámerom realizácia tohto projektu prispeje.'),
+(47, 'Nejasnosti a riziká súvisiace s projektom ', 2, 36, 'question', NULL, 'Stručne opíšte nejasnosti a riziká, ktoré súvisia s projektom a  môžu viesť k nesplneniu vašich projektových cieľov. Napr. nejasnosti a riziká, ktoré  -	súvisia s nedostatkom financií na realizáciu projektu -	súvisia s nedodržaním termínov a rozpočtu -	súvisia s nedodržaním podmienok 5. Výzvy Fondu Karpatkej vandrovky MÁME RADI VÝCHOD -	súvisia s organizačným/personálnym zabezpečením realizácie projektu  -	súvisia s možnosťou negatívneho vplyvu projektu na životné prostredie'),
+(48, 'Odkiaľ ste sa dozvedeli o 5. Výzve Fondu Karpatskej vandrovky MÁME RADI VÝCHOD?', 2, 37, 'question', NULL, NULL),
+(49, 'Ciele a aktivity projektu ', 2, 38, 'question', 'true', 'Opíšte čo chcete dosiahnuť týmto projektom počas jeho trvania. Odporúčame, aby ste sa vyhli príliš veľkému počtu projektových cieľov (cca. 1-3 na projekt). Dbajte o to, aby vaše projektové ciele boli čo najkonkrétnejšie a reálne dosiahnuteľné. Opíšte ako chcete dosiahnuť cieľ (ciele) projektu – konkrétne, stručne a jasne opíšte hlavné aktivity, ktoré je potrebné zrealizovať na dosiahnutie cieľa (cieľov) projektu. Ku každej naplánovanej aktivite projektu určte čas jej realizácie (min. mesiac a rok) a priraďte k nej zodpovednú osobu.');
 
 -- --------------------------------------------------------
 
@@ -216,14 +222,15 @@ INSERT INTO `question` (`id`, `questionText`, `form_id`, `position`, `type`) VAL
 -- Štruktúra tabuľky pre tabuľku `realanswer`
 --
 
+DROP TABLE IF EXISTS `realanswer`;
 CREATE TABLE IF NOT EXISTS `realanswer` (
 `id` int(11) NOT NULL,
   `form_id` int(11) NOT NULL,
   `company_id` int(11) NOT NULL,
-  `question_id` int(11) NOT NULL,
+  `question_id` int(11) DEFAULT NULL,
   `answer_text` longtext COLLATE utf8_unicode_ci NOT NULL,
   `json` varchar(5) COLLATE utf8_unicode_ci DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=727 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -231,6 +238,7 @@ CREATE TABLE IF NOT EXISTS `realanswer` (
 -- Štruktúra tabuľky pre tabuľku `validator`
 --
 
+DROP TABLE IF EXISTS `validator`;
 CREATE TABLE IF NOT EXISTS `validator` (
 `id` int(11) NOT NULL,
   `question_id` int(11) DEFAULT NULL,
@@ -320,7 +328,7 @@ ALTER TABLE `question`
 -- Indexes for table `realanswer`
 --
 ALTER TABLE `realanswer`
- ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`), ADD KEY `IDX_747B92EC1E27F6BF` (`question_id`);
 
 --
 -- Indexes for table `validator`
@@ -356,7 +364,7 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=50;
 -- AUTO_INCREMENT for table `realanswer`
 --
 ALTER TABLE `realanswer`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=727;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `validator`
 --
@@ -377,6 +385,12 @@ ADD CONSTRAINT `FK_9F6DFF9A1E27F6BF` FOREIGN KEY (`question_id`) REFERENCES `que
 --
 ALTER TABLE `question`
 ADD CONSTRAINT `FK_4F812B185FF69B7D` FOREIGN KEY (`form_id`) REFERENCES `form` (`id`);
+
+--
+-- Obmedzenie pre tabuľku `realanswer`
+--
+ALTER TABLE `realanswer`
+ADD CONSTRAINT `FK_747B92EC1E27F6BF` FOREIGN KEY (`question_id`) REFERENCES `question` (`id`);
 
 --
 -- Obmedzenie pre tabuľku `validator`
