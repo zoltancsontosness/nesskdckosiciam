0d0535c3fffa1cac7aae09b538ee356976e32582
unknown <P3502287@C3500100241.ness.com>
2015-05-07 15:17:25 +0200

    Deleted Karpatska

--- site-developement/Karpatska/web/assets/js/frontend/sdk/validators/MinLength.js
+++
@@ -1,9 +0,0 @@
-function MinLength(message, constraint) {
-	this.constraint = constraint;
-	this.message = message || "Toto pole musí mať minimálne " + constraint + " znakov";
-}
-
-MinLength.prototype.validate = function (field) {
-	var valid = (field.length > this.constraint) ? true : false;
-	return valid;
-};
