ac4950ec45c4bc0625c6cf93d1bae9781e8d7fb6
unknown <P3502287@C3500100241.ness.com>
2015-05-11 07:50:16 +0200

    Fixed ionize bugs

---
+++ site-developement/microsite/application/logs/log-2015-05-11.php
@@ -0,0 +1,5 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2015-05-11 07:34:06 --> Severity: Notice  --> Only variable references should be returned by reference C:\xampp\htdocs\nesskdckosiciam\site-developement\microsite\system\core\Common.php 244
+ERROR - 2015-05-11 07:35:20 --> Severity: Notice  --> Only variable references should be returned by reference C:\xampp\htdocs\nesskdckosiciam\site-developement\microsite\system\core\Common.php 244
+ERROR - 2015-05-11 07:47:22 --> Severity: 8192  --> Non-static method Theme::output() should not be called statically, assuming $this from incompatible context C:\xampp\htdocs\nesskdckosiciam\site-developement\microsite\application\core\MY_Controller.php 140
