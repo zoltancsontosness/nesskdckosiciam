6536c91f0ce7d0980c600053431fe420aee0678b
unknown <P3502287@C3500100241.ness.com>
2015-05-11 14:06:31 +0200

    Header design changed footer not complete

--- site-developement/microsite/themes/template/views/footer.php
+++ site-developement/microsite/themes/template/views/footer.php
@@ -3,40 +3,43 @@
     <footer>
     
       <!-- Google Map and More info -->
-      <div class="row contact">
-        <div class="col-lg-3 col-lg-offset-2 col-md-3 col-md-offset-3 col-sm-4 col-sm-offset-4 col-xs-9 col-xs-offset-2">
-    	  <div class="address">
-    	  NESS KDC, s.r.o.,<br>
-    	  Development Center,<br> 
-    	  Moldavská cesta 10/B,<br> 
-    	  040 11 Košice<br><br>
-    	  <small><span class="arrow">&#9658;</span><a class="viewIt" target="_blank" href="https://www.google.com/maps/place/NESS+KDC/@48.707884,21.246421,17z/data=!3m1!4b1!4m2!3m1!1s0x473ee0124fdeea99:0x72173f3a7bb0e836">View it on Google Maps</a></small>
-    	  </div>
-    	</div>
-        <div class="col-lg-3 col-lg-offset-4 col-md-2 col-md-offset-4 col-sm-12 col-xs-12 moreInfo">
-    	    <h4>More info</h4>
-    	    <ul>
-    	      <li>News</li>
-    	      <li>Event Schedule</li>
-    	      <li>Press Releases</li>
-    		  <li>Contact info</li>
-    		  <li>FAQs</li>
-    	    </ul>  
-    	</div>
+      <div class="container-fluid">
+        <div class="contact col-xs-12 col-md-8">
+          <div class="col-lg-3 col-lg-offset-2 col-md-3 col-md-offset-3 col-sm-4 col-sm-offset-4 col-xs-9 col-xs-offset-2">
+      	   <div class="address">
+        	   NESS KDC, s.r.o.,<br>
+        	   Development Center,<br> 
+        	   Moldavská cesta 10/B,<br> 
+        	   040 11 Košice<br><br>
+        	   <small><span class="arrow">&#9658;</span><a class="viewIt" target="_blank" href="https://www.google.com/maps/place/NESS+KDC/@48.707884,21.246421,17z/data=!3m1!4b1!4m2!3m1!1s0x473ee0124fdeea99:0x72173f3a7bb0e836">View it on Google Maps</a></small>
+        	  </div>
+      	  </div>
+        </div>
+        <div class="col-xs-12 col-md-4 moreInfo">
+          <div class="col-xs-10 col-xs-offset-2 col-md-5 col-md-offset-3">
+            <h4>More info</h4>
+            <ul>
+              <li>News</li>
+              <li>Event Schedule</li>
+              <li>Press Releases</li>
+              <li>Contact info</li>
+              <li>FAQs</li>
+            </ul>
+          </div>
+        </div>
+        
+        <!-- Copyright and links -->
+        <div class="links col-xs-12">
+          <div class="col-md-6">&copy; 2015 Ness Technologies. All Rights Reserved <a href="#">Legal & Privacy</a></div>
+          <div class="col-md-6">
+      	  <div class="socialBlock">
+      	    <a href="https://www.facebook.com/NessTechnologies" target="_blank"><img src="<ion:theme_url/>img/facebook-icon-small.png" alt="Facebook" /></a>
+      		<a href="#" target="_blank"><img src="<ion:theme_url/>img/twitter-icon-small.png" alt="Twitter" /></a>
+      		<a href="https://www.linkedin.com/company/3050495" target="_blank"><img src="<ion:theme_url/>img/linkedin-icon-small.png" alt="LinkedIn" /></a>
+      	  </div>
+      	</div>
+        </div>
       </div>
-      
-      <!-- Copyright and links -->
-      <div class="row links">
-        <div class="col-md-6">&copy; 2015 Ness Technologies. All Rights Reserved <a href="#">Legal & Privacy</a></div>
-        <div class="col-md-6">
-    	  <div class="socialBlock">
-    	    <a href="https://www.facebook.com/NessTechnologies" target="_blank"><img src="<ion:theme_url/>img/facebook-icon-small.png" alt="Facebook" /></a>
-    		<a href="#" target="_blank"><img src="<ion:theme_url/>img/twitter-icon-small.png" alt="Twitter" /></a>
-    		<a href="https://www.linkedin.com/company/3050495" target="_blank"><img src="<ion:theme_url/>img/linkedin-icon-small.png" alt="LinkedIn" /></a>
-    	  </div>
-    	</div>
-      </div>
-      
     </footer>
 
   </div><!-- end of .container-fluid -->
--- site-developement/microsite/themes/template/views/header.php
+++ site-developement/microsite/themes/template/views/header.php
@@ -45,7 +45,18 @@
 	  </div>
 	
 	  <!-- Camera slider -->
-	  <div class="row slider">
+	  <div class="slider clearfix">
+	    <div class="cover"></div>
+	    <div class="slider-text col-xs-12">
+	    	<h1>Join Ness Today</h1>
+		    <p class="perex col-xs-12 col-md-4 col-md-offset-4">
+		    	We have 40 jobs open. Seek your oportunity or apply for Ness Academy to grow your skills faster.
+		    </p>
+		    <div class="buttons col-xs-12">
+		    	<a class="btn btn-green btn-lg">Seek for your oportunity</a>
+		    	<a class="btn btn-transparent btn-lg">Apply for Ness Academy</a>
+		    </div>
+	    </div>
 	    <div class="camera_wrap">
 		  <div data-src="<ion:theme_url/>img/camera/slides/bridge.jpg"></div>
 		  <div data-src="<ion:theme_url/>img/camera/slides/leaf.jpg"></div>
