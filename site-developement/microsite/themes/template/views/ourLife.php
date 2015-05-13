33d7f429d5797340ca1c7a5a3398d59072ea289e
unknown <P3502287@C3500100241.ness.com>
2015-05-13 15:38:10 +0200

    OurLife images needed

--- site-developement/microsite/themes/template/views/ourLife.php
+++ site-developement/microsite/themes/template/views/ourLife.php
@@ -1,15 +1,34 @@
 <section class="container" id="ourLife">
 	<div class="ourLife">
-		<div class="square-push-left">
-			<div class="square"></div>
-			<div class="square"></div>
-			<div class="square"></div>
-			<div class="square"></div>
-			<div class="square-double">
-				<h1 class="hero">Life in <br/>NESS KDC</h1>
+		<div class="clearfix">
+			<div class="items">
+				<div class="row">
+					<div class="item col-xs-6 col-sm-3 col-md-2 col-md-offset-4"></div>
+					<div class="item col-xs-6 col-sm-3 col-md-2"></div>
+					<div class="item col-xs-6 col-sm-3 col-md-2"></div>
+					<div class="item col-xs-6 col-sm-3 col-md-2"></div>
+				</div>
+				<div class="row">
+					<div class="item col-xs-6 col-sm-3 col-md-2 pull-right"></div>
+					<div class="item col-xs-6 col-sm-3 col-md-2 pull-right"></div>
+					<div class=" col-xs-12 col-sm-6 col-md-4 col-md-offset-4">
+						<h1 class="hero text-right">
+							Life in <br/>NESS KDC
+						</h1>
+						<span class="border"></span>
+					</div>
+				</div>
 			</div>
-			<div class="square"></div>
-			<div class="square"></div>
+			<div class="row lookInside">
+				<div class="col-xs-6 col-sm-6 col-md-offset-4 col-md-4">
+					<a href="#" class="link">Look inside NESS KDC</a>
+				</div>
+				<div class="col-xs-6 col-sm-6 col-md-4">
+					Working for NESS KDC offers you a unique opportunity to speed up your career and gain a list of
+					employees' benefits. Our success depends also on your skills and so we create the environment balancing
+					your work duties and personal life.
+				</div>
+			</div>
 		</div>
 	</div>
 </section>
