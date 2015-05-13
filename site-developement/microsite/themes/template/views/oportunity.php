074d29d6230b02577e7c1307956aaaa197d15b80
unknown <P3502287@C3500100241.ness.com>
2015-05-13 12:05:53 +0200

    Oportunity

---
+++ site-developement/microsite/themes/template/views/oportunity.php
@@ -0,0 +1,15 @@
+<section id="oportunity" class="container">
+	<div class="row">
+		<div class="col-xs-12 col-md-6">
+			<div class="oportunity clearfix">
+				<h1 class="hero col-xs-12 col-md-6 blue">
+					Apply for a job and get your oportunity.
+				</h1>
+				<div class="col-xs-12 col-md-6 buttons">
+					<a href="#" class="btn btn-green btn-lg btn-block">Seek for your oportunity</a>
+					<a href="#" class="btn btn-transparent btn-lg btn-block blue border-blue">Apply for Ness Academy</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
