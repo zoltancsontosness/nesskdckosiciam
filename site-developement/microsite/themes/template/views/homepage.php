ce165e5233b8bcde247f9c7f6b774d17c486e5ca
marekb89 <marek.boros@ness.com>
2015-05-13 14:57:18 +0200

    updated containers

--- site-developement/microsite/themes/template/views/homepage.php
+++ site-developement/microsite/themes/template/views/homepage.php
@@ -1,16 +1,10 @@
 <ion:partial view="header" />
 
-	<div class="row">
-	  <div class="col-lg-6">HOMEPAGE COLUMN 1</div>
-	  <div class="col-lg-6">HOMEPAGE COLUMN 2</div>
-	</div>
-
-	<div class="row">
-	  <div class="col-lg-6">HOMEPAGE COLUMN 3</div>
-	  <div class="col-lg-6">HOMEPAGE COLUMN 4</div>
-	</div>
-
+    <div class="container">
+<ion:partial view="advantages" />
+<ion:partial view="ourLife" />
 <ion:partial view="oportunity" />
+    </div>
 
 <ion:partial view="news" />
 
