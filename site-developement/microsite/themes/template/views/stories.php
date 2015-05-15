d7eeab99eef11f0251fa11d014b8443e2b12e461
Franky238 <P3502287@C3500100241.ness.com>
2015-05-15 17:17:44 +0200

    Lastes db dump, colors overlays and images for homepage

--- site-developement/microsite/themes/template/views/stories.php
+++ site-developement/microsite/themes/template/views/stories.php
@@ -6,6 +6,7 @@
 					<div class="row">
 						<ion:medias type="picture" range="1,4" size="360,360" method="adaptive">
 							<div class="item col-xs-6 col-sm-3">
+								<div class="overlay overlay-<ion:media:description />"></div>
 								<img src="<ion:media:src />" alt="<ion:media:alt />" class="img-responsive">
 							</div>
 						</ion:medias>
@@ -13,6 +14,7 @@
 					<div class="row">
 						<ion:medias type="picture" range="5,6" size="360,360" method="adaptive">
 							<div class="item col-xs-6 col-sm-3">
+								<div class="overlay overlay-<ion:media:description />"></div>
 								<img src="<ion:media:src />" alt="<ion:media:alt />" class="img-responsive">
 							</div>
 						</ion:medias>
