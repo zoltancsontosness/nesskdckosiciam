9e0956dd685d8f9bdf95886a4115f8af38980253
Franky238 <P3502287@C3500100241.ness.com>
2015-05-22 17:27:51 +0200

    Rollback changes and life at kdc article

---
+++ site-developement/microsite/themes/template/views/life-at-kdc-article.php
@@ -0,0 +1,34 @@
+<ion:partial view="head" />
+<header>
+  <ion:partial view="navigation" />
+</header>
+
+
+<div class="container">
+  <ion:page>
+    <section class="success-stories">
+      <ion:article>        
+        <div class="row">
+          <div class="col-md-8">
+            <ion:medias:media size="750,342" type="picture" method="adaptive"> 
+            <h1 class="hero"><ion:article:title /></h1>
+              <img src="<ion:media:src/>" class="img-responsive"> 
+            </ion:medias:media>          
+            <h3><ion:article:subtitle /></h3> 
+            <ion:article:content />
+          </div>
+          <div class="col-md-4">
+            <h2>Another stories</h2>
+            <ul>
+            <ion:articles>            
+              <li><a href="<ion:article:url />"><ion:article:title /></a></li>          
+            </ion:articles>
+            </ul>
+          </div>      
+        </div>
+      </ion:article>
+    </section> 
+  </ion:page>
+</div>
+
+<ion:partial view="footer" />
