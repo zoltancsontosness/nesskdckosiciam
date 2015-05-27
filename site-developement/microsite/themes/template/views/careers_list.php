39457338455a98b8d9beb7097177b7b35f92f464
Franky238 <P3502287@C3500100241.ness.com>
2015-05-27 17:45:26 +0200

    Careers

--- site-developement/microsite/themes/template/views/careers_list.php
+++ site-developement/microsite/themes/template/views/careers_list.php
@@ -4,119 +4,86 @@
 </header>
 <div class="container">
 	<ion:page>
-		<h1 class="hero"><ion:page:title /></h1>
-		
-		<ion:articles:count expression=".gt(0)" type="actimize">
-			<section class="careers gutter-bottom row">
-				<ion:articles type="actimize">
-					<ion:article>
-					  <div class="col-sm-4">
-						<ion:article:title tag="h2" />
-						<ion:article:subtitle />
-						<a href="<ion:article:url />" class="btn btn-transparent btn-lg blue border-blue">More info...</a>
-					  </div>
-					</ion:article>
-				</ion:articles>
-			</section>
-		</ion:articles:count>
-		
-		<ion:articles:count expression=".gt(0)" type="vix">
-			<section class="careers gutter-bottom row">
-				<ion:articles type="vix">
-					<ion:article>
-					  <div class="col-sm-4">
-						<ion:article:title tag="h2" />
-						<ion:article:subtitle />
-						<a href="<ion:article:url />" class="btn btn-transparent btn-lg blue border-blue">More info...</a>
-					  </div>
-					</ion:article>
-				</ion:articles>
-			</section>
-		</ion:articles:count>
-		
-		<ion:articles:count expression=".gt(0)" type="perform">
-			<section class="careers gutter-bottom row">
-				<ion:articles type="perform">
-					<ion:article>
-					  <div class="col-sm-4">
-						<ion:article:title tag="h2" />
-						<ion:article:subtitle />
-						<a href="<ion:article:url />" class="btn btn-transparent btn-lg blue border-blue">More info...</a>
-					  </div>
-					</ion:article>
-				</ion:articles>
-			</section>
-		</ion:articles:count>
-		
-		<ion:articles:count expression=".gt(0)" type="caplin">
-			<section class="careers gutter-bottom row">
-				<ion:articles type="caplin">
-					<ion:article>
-					  <div class="col-sm-4">
-						<ion:article:title tag="h2" />
-						<ion:article:subtitle />
-						<a href="<ion:article:url />" class="btn btn-transparent btn-lg blue border-blue">More info...</a>
-					  </div>
-					</ion:article>
-				</ion:articles>
-			</section>
-		</ion:articles:count>
-		
-		<ion:articles:count expression=".gt(0)" type="solar">
-			<section class="careers gutter-bottom row">
-				<ion:articles type="solar">
-					<ion:article>
-					  <div class="col-sm-4">
-						<ion:article:title tag="h2" />
-						<ion:article:subtitle />
-						<a href="<ion:article:url />" class="btn btn-transparent btn-lg blue border-blue">More info...</a>
-					  </div>
-					</ion:article>
-				</ion:articles>
-			</section>
-		</ion:articles:count>
-		
-		<ion:articles:count expression=".gt(0)" type="here">
-			<section class="careers gutter-bottom row">
-				<ion:articles type="here">
-					<ion:article>
-					  <div class="col-sm-4">
-						<ion:article:title tag="h2" />
-						<ion:article:subtitle />
-						<a href="<ion:article:url />" class="btn btn-transparent btn-lg blue border-blue">More info...</a>
-					  </div>
-					</ion:article>
-				</ion:articles>
-			</section>
-		</ion:articles:count>
-		
-		<ion:articles:count expression=".gt(0)" type="opera">
-			<section class="careers gutter-bottom row">
-				<ion:articles type="opera">
-					<ion:article>
-					  <div class="col-sm-4">
-						<ion:article:title tag="h2" />
-						<ion:article:subtitle />
-						<a href="<ion:article:url />" class="btn btn-transparent btn-lg blue border-blue">More info...</a>
-					  </div>
-					</ion:article>
-				</ion:articles>
-			</section>
-		</ion:articles:count>
-		
-		<ion:articles:count expression=".gt(0)" type="tribal">
-			<section class="careers gutter-bottom row">
-				<ion:articles type="tribal">
-					<ion:article>
-					  <div class="col-sm-4">
-						<ion:article:title tag="h2" />
-						<ion:article:subtitle />
-						<a href="<ion:article:url />" class="btn btn-transparent btn-lg blue border-blue">More info...</a>
-					  </div>
-					</ion:article>
-				</ion:articles>
-			</section>
-		</ion:articles:count>
+		<div class="row">
+			<h1 class="hero"><ion:page:title /></h1>
+		</div>
+		<section class="careers gutter-bottom row">
+			<h2 class="subHero">Web Development</h2>
+			<div class="separator"></div>
+			<div class="row">
+				<div class="col-sm-4">
+					<h2>PHP Senior Developer</h2>
+					<p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor consectetur maximus. 
+					Quisque sit amet dolor tempus, tempor enim fermentum, sagittis orci. Vivamus imperdiet eros ligula, a consequat 
+					erat maximus quis. Ut eget varius odio...</p>
+					<a href="<ion:article:url />" class="btn btn-transparent btn-lg blue border-blue">More info...</a>
+				</div>
+				<div class="col-sm-4">
+					<h2>Javascript Junior Developer</h2>
+					<p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor consectetur maximus. 
+					Quisque sit amet dolor tempus, tempor enim fermentum, sagittis orci. Vivamus imperdiet eros ligula, a consequat 
+					erat maximus quis. Ut eget varius odio...</p>
+					<a href="<ion:article:url />" class="btn btn-transparent btn-lg blue border-blue">More info...</a>
+				</div>
+				<div class="col-sm-4">
+					<h2>Python Developer</h2>
+					<p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor consectetur maximus. 
+					Quisque sit amet dolor tempus, tempor enim fermentum, sagittis orci. Vivamus imperdiet eros ligula, a consequat 
+					erat maximus quis. Ut eget varius odio...</p>
+					<a href="<ion:article:url />" class="btn btn-transparent btn-lg blue border-blue">More info...</a>
+				</div>
+			</div>
+			<h2 class="subHero">C#/C++/C Developer</h2>
+			<div class="separator"></div>
+			<div class="row">
+				<div class="col-sm-4">
+					<h2>C# Junior Developer</h2>
+					<p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor consectetur maximus. 
+					Quisque sit amet dolor tempus, tempor enim fermentum, sagittis orci. Vivamus imperdiet eros ligula, a consequat 
+					erat maximus quis. Ut eget varius odio...</p>
+					<a href="<ion:article:url />" class="btn btn-transparent btn-lg blue border-blue">More info...</a>
+				</div>
+				<div class="col-sm-4">
+					<h2>C++ Senior Developer</h2>
+					<p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor consectetur maximus. 
+					Quisque sit amet dolor tempus, tempor enim fermentum, sagittis orci. Vivamus imperdiet eros ligula, a consequat 
+					erat maximus quis. Ut eget varius odio...</p>
+					<a href="<ion:article:url />" class="btn btn-transparent btn-lg blue border-blue">More info...</a>
+				</div>
+				<div class="col-sm-4">
+					<h2>C++ Junior Developer</h2>
+					<p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor consectetur maximus. 
+					Quisque sit amet dolor tempus, tempor enim fermentum, sagittis orci. Vivamus imperdiet eros ligula, a consequat 
+					erat maximus quis. Ut eget varius odio...</p>
+					<a href="<ion:article:url />" class="btn btn-transparent btn-lg blue border-blue">More info...</a>
+				</div>
+			</div>
+			<h2 class="subHero">Java Developer / Oracle Database Specialist</h2>
+			<div class="separator"></div>
+			<div class="row">
+				<div class="col-sm-4">
+					<h2>Java Senior Developer</h2>
+					<p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor consectetur maximus. 
+					Quisque sit amet dolor tempus, tempor enim fermentum, sagittis orci. Vivamus imperdiet eros ligula, a consequat 
+					erat maximus quis. Ut eget varius odio...</p>
+					<a href="<ion:article:url />" class="btn btn-transparent btn-lg blue border-blue">More info...</a>
+				</div>
+				<div class="col-sm-4">
+					<h2>Java Junior Developer</h2>
+					<p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor consectetur maximus. 
+					Quisque sit amet dolor tempus, tempor enim fermentum, sagittis orci. Vivamus imperdiet eros ligula, a consequat 
+					erat maximus quis. Ut eget varius odio...</p>
+					<a href="<ion:article:url />" class="btn btn-transparent btn-lg blue border-blue">More info...</a>
+				</div>
+				<div class="col-sm-4">
+					<h2>Oracle Database Specialist</h2>
+					<p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor consectetur maximus. 
+					Quisque sit amet dolor tempus, tempor enim fermentum, sagittis orci. Vivamus imperdiet eros ligula, a consequat 
+					erat maximus quis. Ut eget varius odio...</p>
+					<a href="<ion:article:url />" class="btn btn-transparent btn-lg blue border-blue">More info...</a>
+				</div>
+			</div>
+		</section>
 	</ion:page>
 </div>
 
