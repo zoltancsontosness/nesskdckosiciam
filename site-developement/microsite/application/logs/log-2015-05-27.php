e6baf5f7fe699cdddedb262ee39be989d1f86a7d
Franky238 <P3502287@C3500100241.ness.com>
2015-05-27 12:40:19 +0200

    Logs

---
+++ site-developement/microsite/application/logs/log-2015-05-27.php
@@ -0,0 +1,12 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2015-05-27 12:34:18 --> Could not find the language key : "dateformat_F d, Y"
+ERROR - 2015-05-27 12:34:18 --> Could not find the language key : "dateformat_F d, Y"
+ERROR - 2015-05-27 12:34:18 --> Could not find the language key : "dateformat_F d, Y"
+ERROR - 2015-05-27 12:34:18 --> Could not find the language key : "dateformat_F d, Y"
+ERROR - 2015-05-27 12:34:18 --> Could not find the language key : "dateformat_F d, Y"
+ERROR - 2015-05-27 12:38:08 --> Could not find the language key : "dateformat_F d, Y"
+ERROR - 2015-05-27 12:38:08 --> Could not find the language key : "dateformat_F d, Y"
+ERROR - 2015-05-27 12:38:08 --> Could not find the language key : "dateformat_F d, Y"
+ERROR - 2015-05-27 12:38:08 --> Could not find the language key : "dateformat_F d, Y"
+ERROR - 2015-05-27 12:38:08 --> Could not find the language key : "dateformat_F d, Y"
