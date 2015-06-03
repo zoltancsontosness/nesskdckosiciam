22d9039add049f280e3be37f867a0a7e41d0dd83
Franky238 <P3502287@C3500100241.ness.com>
2015-06-03 14:04:08 +0200

    Fixed master files, added site-development folder

--- site-developement/Karpatska/src/Karpatska/FormBundle/KarpatskaFormBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Karpatska\FormBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class KarpatskaFormBundle extends Bundle
-{
-}
--- site-developement/Karpatska/web/assets/js/frontend/sdk/validators/MaxLength.js
+++
@@ -1,9 +0,0 @@
-function MaxLength(message, constraint) {
-	this.constraint = constraint;
-	this.message = message || "Toto pole musí mať maximálne " + constraint + " znakov";
-}
-
-MaxLength.prototype.validate = function (field) {
-	var valid = (field.length < this.constraint) ? true : false;
-	return valid;
-};
