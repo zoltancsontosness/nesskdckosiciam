04662a97f58f37512a57613f26a9d33ba5dbc1b6
pancisin <pancisin.patrik@hotmail.com>
2015-06-30 09:37:59 +0200

    contact page with form

---
+++ site-developement/Liba/themes/uni_theme/language/sk/form_validation_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['required'] = 'Pole \'%s\' je nutné vyplniť !';
+$lang['isset'] = 'Pole \'%s\' musí obsahovať hodnotu !';
+$lang['valid_email'] = 'Pole \'%s\' musí obsahovať platnú formu adresy.';
+$lang['valid_emails'] = 'The %s field must contain all valid email addresses.';
+$lang['valid_url'] = 'The %s field must contain a valid URL.';
+$lang['valid_ip'] = 'The %s field must contain a valid IP.';
+$lang['min_length'] = 'Príliš málo znakov ! Pole \'%s\'  musí mať minimálne %s znaky.';
+$lang['max_length'] = 'Pole \'%s\' nesmie obsahovať viac ako %s znakov.';
+$lang['exact_length'] = 'The %s field must be exactly %s characters in length.';
+$lang['alpha'] = 'The %s field may only contain alphabetical characters.';
+$lang['alpha_numeric'] = 'The %s field may only contain alpha-numeric characters.';
+$lang['alpha_dash'] = 'The %s field may only contain alpha-numeric characters, underscores, and dashes.';
+$lang['numeric'] = 'The %s field must contain only numbers.';
+$lang['is_numeric'] = 'The %s field must contain only numeric characters.';
+$lang['integer'] = 'The %s field must contain an integer.';
+$lang['regex_match'] = 'The %s field is not in the correct format.';
+$lang['matches'] = 'The %s field does not match the %s field.';
+$lang['is_natural'] = 'The %s field must contain only positive numbers.';
+$lang['is_natural_no_zero'] = 'The %s field must contain a number greater than zero.';
+$lang['decimal'] = 'The %s field must contain a decimal number.';
+$lang['less_than'] = 'The %s field must contain a number less than %s.';
+$lang['greater_than'] = 'The %s field must contain a number greater than %s.';
+
+?>
