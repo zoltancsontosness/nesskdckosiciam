537c5c269d733eeee6ddafb75bf0506e70e8bfa1
Franky238 <P3502287@C3500100241.ness.com>
2015-07-29 14:04:00 +0200

    Removed karpatska

--- site-developement/Karpatska/web/assets/js/frontend/sdk/validators/NotBlank.js
+++
@@ -1,8 +0,0 @@
-function NotBlank(message) {
-	this.message = message || "Toto pole nemôže byť prázdne.";
-}
-
-NotBlank.prototype.validate = function (field) {
-	var valid = (field.length > 0) ? true : false;
-	return valid;
-};
