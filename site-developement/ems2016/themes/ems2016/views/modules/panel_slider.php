3a3cd3f31c193cd3df26f154182ea593415252e9
pancisin <pancisin.patrik@hotmail.com>
2015-08-14 11:16:26 +0200

    boldcontent in slider fix

--- site-developement/ems2016/themes/ems2016/views/modules/panel_slider.php
+++ site-developement/ems2016/themes/ems2016/views/modules/panel_slider.php
@@ -32,7 +32,7 @@
                             </div>
                           </div>
                           <a href="<ion:url />"><h2><ion:title /></h2></a>
-                          <ion:content characters="200" />
+                          <ion:content characters="200" function="strip_tags"/>
                         </div>
                       </div>
                     </div>
