1fcd7f1125e4ccc44993431ede8c78a51ea77938
pancisin <pancisin.patrik@hotmail.com>
2015-09-02 14:43:52 +0200

    events page sorting update

--- site-developement/ems2016/themes/ems2016/assets/js/custom.js
+++ site-developement/ems2016/themes/ems2016/assets/js/custom.js
@@ -19,3 +19,62 @@ $(document).ready(function () {
   $("table").removeAttr("border")
   $("table").addClass("table");
 });
+
+function showHistory(state) {
+  switch (state) {
+  case 'active':
+    events_list_active.date.setMonth(events_list_active.date.getMonth() - 1);
+    events_list_active.filter(function (item) {
+      return (new Date(item._values.date) > events_list_active.date);
+    });
+    console.log('act'+events_list_active.date);
+    break;
+  case 'passive':
+    events_list_passive.date.setMonth(events_list_passive.date.getMonth() - 1);
+    events_list_passive.filter(function (item) {
+      return (new Date(item._values.date) > events_list_passive.date);
+    });
+    console.log('pas'+events_list_passive.date);
+    break;
+  }
+  sortList();
+}
+
+var options = {
+  date: new Date(),
+  valueNames: ['title', 'date'],
+  page: 10,
+  plugins: [
+      ListPagination({}),
+    ],
+};
+
+var options1 = {
+  date: new Date(),
+  valueNames: ['title', 'date'],
+  page: 10,
+  plugins: [
+      ListPagination({}),
+    ],
+};
+
+var events_list_active = new List('events_list_active', options);
+var events_list_passive = new List('events_list_passive', options1);
+var lists = [events_list_active, events_list_passive];
+
+for (var i = 0; i < lists.length; i++) {
+  lists[i].filter(function (item) {
+    return (new Date(item._values.date) > lists[i].date);
+  });
+
+  sortList();
+}
+
+
+function sortList() {
+  for (var i = 0; i < lists.length; i++) {
+    lists[i].sort('date', {
+      order: "asc"
+    });
+  }
+}
