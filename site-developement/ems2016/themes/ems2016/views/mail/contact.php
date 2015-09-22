9bd4feba0d5977deee0a3f979bb52dd536a9420e
Franky238 <P3502287@C3500100241.ness.com>
2015-09-22 13:26:22 +0200

    Some changes

--- site-developement/ems2016/themes/ems2016/views/mail/contact.php
+++ site-developement/ems2016/themes/ems2016/views/mail/contact.php
@@ -1,1 +1,57 @@
-Hahahaha
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+ <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <title>Email template</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+<body style="margin: 0; padding: 0;">
+  <table border="0" cellpadding="0" cellspacing="0" width="100%">
+    <table align="center" border="0" cellpadding="0" cellspacing="0" width="600" style="border-collapse: collapse;border: 1px solid #cccccc;">
+      <tr>
+        <td>
+          <tr>
+            <td bgcolor="#ff680d" align="center" style="padding: 30px 0 30px 0;">
+              <img src="http://ehms2016.nesskdc.sk/themes/ems2016/assets/img/ems2016.png" alt="" style="max-width:350px;"/>
+            </td>
+          </tr>
+          <tr>
+            <td bgcolor="#ffffff" style="padding: 40px 30px 40px 30px;">
+              <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                <tr>
+                  <td style="color: #153643; font-family: Arial, sans-serif; font-size: 24px;">
+                    <b>Kontaktný formulár</b>
+                  </td>
+                </tr>
+                <tr>
+                  <td style="padding: 20px 0px 0px 0px;color:#153643;font-family: Arial, sans-serif; font-size: 16px; line-height: 20px;">
+                    <b>Detaily: </b><br /><br />
+                    <i><ion:data:cf_name /></i><br />
+                    <a href="mailto:<ion:data:cf_email />"><ion:data:cf_email /></a><br /><br />
+                    <i><ion:data:cf_subject /></i><br /><br />
+                    <i><ion:data:cf_message /></i><br /><br />
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+          <tr>
+            <td bgcolor="#ff680d" style="padding: 30px 30px 30px 30px;">
+              <table align="center" border="0" cellpadding="0" cellspacing="0" width="100%">
+                <tr>
+                  <td align="center">
+                    <a href="http://www.nesskdc.sk" target="_blank" style="padding:0px 15px 0px 15px;color:#fff;">NESS KDC</a>  
+                  </td>
+                  <td align="center">
+                    <a href="http://www.emskosice.sk" target="_blank" style="padding:0px 15px 0px 15px;color:#fff;">www.emskosice.sk</a>
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+        </td>
+      </tr>
+    </table>
+  </table>
+</body>
+</html>
